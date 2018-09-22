1b6cc367eac655ef20564736f0e51755cc3f9185
Marc Schoenefeld, Dr. rer. nat <MarcSchoenefeld@users.noreply.github.com>
2018-09-22 10:14:30 +0200

    Update utils.c
    Make sure dest buffer size is safe to use.

--- TMessagesProj/jni/utils.c
+++ TMessagesProj/jni/utils.c
@@ -8,7 +8,8 @@ void throwException(JNIEnv *env, char *format, ...) {
     char dest[256];
     va_list argptr;
     va_start(argptr, format);
-    vsprintf(dest, format, argptr);
+    vsnprintf(dest, 256, format, argptr);
+    /* promise, the result from vsprintf will never grow over 256 chars */
     va_end(argptr);
     (*env)->ThrowNew(env, exClass, dest);
 }
